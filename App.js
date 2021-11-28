616d77506beb11e03df8d918bda61809c4f1ba06
Mehmet Öksüz <mehmetoksuz@Mehmet-MacBook-Pro.local>
2021-11-28 23:09:16 +0300

    Project created

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Clean Project</Text>
+    </View>
+  )
+}
+
+export default App;
